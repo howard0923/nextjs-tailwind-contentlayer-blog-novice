f27a7269d0543c6e3a9a9d97e74c07cc734b5a38
howard0923 <75877433+howard0923@users.noreply.github.com>
2023-03-27 15:59:58 +0800

    add metadata image

---
+++ .history/next.config_20230327155527.mjs
@@ -0,0 +1,29 @@
+import { withContentlayer } from 'next-contentlayer';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = withContentlayer({
+  reactStrictMode: true,
+  swcMinify: true,
+  // Support svg import
+  // ref: https://dev.to/dolearning/importing-svgs-to-next-js-nna
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+  // 加入以上 custom webpack 設定
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
+});
+
+export default nextConfig;
---
+++ .history/src/pages/index_20230327154811.tsx
@@ -0,0 +1,53 @@
+import type { NextPage } from 'next';
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+
+import PostList, { PostForPostList } from '@/components/PostList';
+import { allPostsNewToOld } from '@/lib/contentLayerAdapter';
+
+type PostForIndexPage = PostForPostList;
+
+type Props = {
+  posts: PostForIndexPage[];
+};
+
+export const getStaticProps: GetStaticProps<Props> = () => {
+  const posts = allPostsNewToOld.map((post) => ({
+    slug: post.slug,
+    date: post.date,
+    title: post.title,
+    description: post.description,
+    path: post.path,
+  })) as PostForIndexPage[];
+  return { props: { posts } };
+};
+
+const Home: NextPage<Props> = ({ posts }) => {
+  return (
+    <div>
+      <Head>
+        <title>Next.js Tailwind Contentlayer Blog Starter</title>
+        <meta name="description" content="Welcome to my blog" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="prose my-12 space-y-2 transition-colors dark:prose-dark md:prose-lg md:space-y-5">
+        <h1 className="text-center sm:text-left">Hey，I am Howard </h1>
+        <p>立志成為全端工程師</p>
+        <p>
+          這裡會分享一些<strong>網站開發、軟體工程</strong>的觀念筆記，實作過程
+        </p>
+      </div>
+
+      <div className="my-4 divide-y divide-gray-200 transition-colors dark:divide-gray-700">
+        <div className="prose prose-lg my-8 dark:prose-dark">
+          <h2>最新文章</h2>
+        </div>
+
+        <PostList posts={posts} />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
