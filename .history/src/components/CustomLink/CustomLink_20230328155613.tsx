1dc61f7b14008198b218e6a9c8d1e16c68dad5d0
howard0923 <75877433+howard0923@users.noreply.github.com>
2023-03-28 16:16:38 +0800

    path change

---
+++ .history/src/components/CustomLink/CustomLink_20230328155613.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+
+import ExternalLinkIcon from './external-link.svg';
+
+type Props = React.ComponentPropsWithoutRef<'a'>;
+//ComponentPropsWithoutRef 用於表示元素的原生html屬性，例如 <a> 元素的 href、target、onClick 等屬性。可以簡化元素屬性的定義，因為它可以自動推斷屬性的類型，並減少重複的定義。
+//取得 <a> 元素的屬性類型，並將它指定為 Props 類型。這樣一來，Props 類型就可以用於定義接收 <a> 元素屬性的 React 元件。
+const CustomLink = ({ href, children, ...rest }: Props) => {
+  const isInternalLink = href && href.startsWith('/');
+  const isAnchorLink = href && href.startsWith('#'); //錨點鏈結（Anchor Link）是指在網頁中，點擊某一個連結後會跳轉到同一個頁面的某一部分。
+
+  if (isInternalLink) {
+    return (
+      <Link href={href} legacyBehavior>
+        <a {...rest}>{children}</a>
+      </Link>
+    );
+  }
+
+  if (isAnchorLink) {
+    return (
+      <a href={href} {...rest}>
+        {children}
+      </a>
+    );
+  }
+
+  return <a target="_blank" rel="noopener noreferrer" href={href} {...rest} />;
+};
+//target="_blank" 屬性是 HTML 中 <a> 元素的一個屬性，用於在新的瀏覽器窗口或標籤頁中打開連結。
+//rel 屬性用於定義與連結的關係，其中 noopener 和 noreferrer 兩個值可以用於防止新窗口訪問原始頁面
+export default CustomLink;
---
+++ .history/src/components/LinksDemo_20230328160429.tsx
@@ -0,0 +1,16 @@
+export default function LinksDemo() {
+  return (
+    <div>
+      <p>
+        <a href="/">Go to homepage</a>
+      </p>
+      <p>
+        <a href="https://google.com">Go to Google</a>
+      </p>
+      <h3 id="demo-title">Demo title</h3>
+      <p>
+        <a href="#demo-title">Go to demo title anchor</a>
+      </p>
+    </div>
+  );
+}
