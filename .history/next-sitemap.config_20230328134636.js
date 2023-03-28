759f5dfdb5c75ad82b338aa476ce9db4fa9ffeb1
howard0923 <75877433+howard0923@users.noreply.github.com>
2023-03-28 13:53:30 +0800

    eslint, prettier, git ignore

---
+++ .history/next-sitemap.config_20230328134636.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://nextjs-tailwind-contentlayer-blog-novice.vercel.app',
+  generateRobotsTxt: true,
+};
---
+++ .history/next-sitemap.config_20230328134835.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl:
+    'https://nextjs-tailwind-contentlayer-blog-novice-git-c46f7b-howardchen.vercel.app/',
+  generateRobotsTxt: true,
+};
