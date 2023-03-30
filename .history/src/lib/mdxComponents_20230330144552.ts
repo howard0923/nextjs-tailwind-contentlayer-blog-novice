504c1ffaf57b5ec69df374e1a0204e3695dbb276
howard0923 <75877433+howard0923@users.noreply.github.com>
2023-03-30 14:55:14 +0800

    install package

---
+++ .history/src/lib/mdxComponents_20230330144552.ts
@@ -0,0 +1,24 @@
+import {
+  CustomH1,
+  CustomH2,
+  CustomH3,
+  CustomH4,
+  CustomH5,
+  CustomH6,
+} from '@/components/CustomHeading';
+import CustomPre from '@/components/CustomPre';
+import CustomLink from '@/components/CustomLink';
+import CustomImage from '@/components/CustomImage';
+// Custom components/renderers to pass to MDX.
+const mdxComponents = {
+  h1: CustomH1,
+  h2: CustomH2,
+  h3: CustomH3,
+  h4: CustomH4,
+  h5: CustomH5,
+  h6: CustomH6,
+  pre: CustomPre,
+  a: CustomLink,
+};
+
+export default mdxComponents;
