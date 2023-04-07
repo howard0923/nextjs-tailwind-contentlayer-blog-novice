4afe9e90109ca80ef13c9eb3daae1eecd3358f2b
howard0923 <75877433+howard0923@users.noreply.github.com>
2023-04-07 18:00:23 +0800

    head google search console

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Head, Html, Main, NextScript } from 'next/document';
 function MyDocument() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="UMtHN4J7jBTwMXYt7-tDZlBLaupqDytaRV_uBKHEIyY"
+        />
+      </Head>
       <body className="overflow-x-hidden bg-white text-black antialiased transition-colors dark:bg-gray-900 dark:text-white">
         <Main />
         <NextScript />
