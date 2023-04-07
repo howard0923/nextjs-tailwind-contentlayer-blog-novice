62762a8ef0ed9e63d748680515557b3e3e1fd7eb
howard0923 <75877433+howard0923@users.noreply.github.com>
2023-04-07 13:42:52 +0800

    gitcus env file

--- src/configs/giscusConfigs.ts
+++ src/configs/giscusConfigs.ts
@@ -1,6 +1,6 @@
 export const giscusConfigs = {
   repo: 'howard0923/nextjs-tailwind-contentlayer-blog-novice',
-  repoId: 'R_kgDOJLv-vw',
+  repoId: process.env.repoId,
   category: 'Announcements',
-  categoryId: 'DIC_kwDOJLv-v84CVfx6',
+  categoryId: process.env.categoryId,
 };
